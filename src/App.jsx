9e635d4815c4785acbc7b5399df8418948f6d2d9
Taras <r.taras.t.1@gmail.com>
2025-04-25 02:20:46 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
 
   return (
     <>
-
+      <p>{import.meta.env.VITE_REACT_APP_UNSPLASH_API_KEY}</p>
     </>
   )
-}
+};
 
 export default App
